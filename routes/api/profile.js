d50d3562dce8b2f772f9924b0f0161cc6168cf62
Emanuele <emanuelemastaglia@gmail.com>
2022-01-14 11:49:56 +0100

    profile route update add save and update profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,11 +1,139 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../../middleware/auth')
+const {check, validationResult} = require("express-validator");
 
-//@route GET api/profile
-//@desc Test route
-//@access Public
-router.get('/',(req,res)=>{
-    res.send('profile route')
+const Profile = require('../../models/Profile');
+const User = require('../../models/User')
+
+//@route GET api/profile/me
+//@desc Get current user profile
+//@access Private
+router.get('/me',auth,async (req,res)=>{
+    try{
+     const profile = await Profile.findOne({user: req.user.id}).populate('user',['name','avatar']);
+
+     if(!profile){
+         return res.status(400).json({msg:"There is no profile for this user"})
+     }
+
+     res.json(profile)
+
+    }catch (err){
+        console.error(err.message);
+        res.status(500).send("Server error")
+    }
 })
+
+//@route POST api/profile
+//@desc Create or update user profile
+//@access Private
+router.post('/',[
+    auth,[
+        check('status',"Status is required").not().isEmpty(),
+        check('skills',"Skills is required").not().isEmpty()
+],
+], async (req,res)=>{
+   const errors = validationResult(req);
+   if(!errors.isEmpty()){
+       return res.status(400).json({ errors: errors.array()})
+   }
+
+    // destructure the request
+    const {
+        company,
+        website,
+        location,
+        bio,
+        status,
+        githubusername,
+        skills,
+        youtube,
+        facebook,
+        twitter,
+        instagram,
+        linkedin
+    } = req.body;
+
+    // Get fields
+    const profileFields = {};
+    profileFields.user = req.user.id;
+    if(company)profileFields.company= company;
+    if(location)profileFields.location= location;
+    if(website)profileFields.website= website;
+    if(bio)profileFields.bio= bio;
+    if(status)profileFields.status= status;
+
+    // Skills - Spilt into array
+    if (skills) {
+        profileFields.skills = skills.split(',').map(skill=>skill.trim())
+    }
+
+    // Build social
+    profileFields.social={};
+    if(youtube)profileFields.social.youtube= youtube;
+    if(twitter)profileFields.social.twitter= twitter;
+    if(linkedin)profileFields.social.linkedin= linkedin;
+    if(instagram)profileFields.social.instagram= instagram;
+
+    try {
+        //Check if profile exists
+       let profile = await Profile.findOne({user: req.user.id})
+
+        if(profile){
+            //update
+            profile = await Profile.findOneAndUpdate({user: req.user.id},
+                {$set: profileFields},
+                {new:true});
+            return res.json(profile);
+        }
+
+        //Create
+        profile = new Profile(profileFields);
+        await profile.save();
+        res.json(profile);
+
+    }catch (err){
+        console.error(err.message);
+        res.status(500).send('Server error')
+    }
+
+
+})
+
+//@route POST api/profile
+//@desc Get all profiles
+//@access Public
+router.get('/',async (req,res)=>{
+    try{
+    const profiles = await Profile.find().populate('user',['name', 'avatar']);
+    res.json(profiles)
+
+    }catch (err){
+        console.error(err.message);
+        res.status(500).send('Server error');
+    }
+})
+
+//@route POST api/profile/user/:user_id
+//@desc Get profile by id
+//@access Public
+router.get('/user/:user_id',async (req,res)=>{
+    try{
+        const profile = await Profile.findOne({user:req.params.user_id}).populate('user',['name', 'avatar']);
+
+        if(!profile) return res.status(400).json({msg: "there is no profile for this user"})
+
+        res.json(profile)
+
+    }catch (err){
+        console.error(err.message);
+        if(err.kind == 'ObjectId'){
+            return res.status(400).json({msg: "profile not found"})
+        }
+        res.status(500).send('Server error');
+    }
+})
+
 
 module.exports = router;
